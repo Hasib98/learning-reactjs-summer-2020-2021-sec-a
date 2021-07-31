f99b4d71e28d59eab890a17e10ea0597f79949ba
Hasib98 <phoenixoffire1998@gmail.com>
2021-07-31 13:02:19 +0600

    refined User add

--- react-test/src/App.js
+++ react-test/src/App.js
@@ -12,14 +12,16 @@ function App() {
     const list = userlist.filter((user)=>user.id !== id);
     setUserList(list);
   }
-
+  const addUser = (user)=>{
+    setUserList([...userlist,user]);
+  }
   return (
    
     <Router>
       <Navbar/>
       <Switch>
           <Route exact path='/'> 
-              <h1>Welcome Home!</h1>
+              <h1> HASIB!</h1>
           </Route>
           <Route path='/userlist'>
             <div>
@@ -27,7 +29,7 @@ function App() {
             </div>
           </Route>
           <Route path='/create'>
-              <CreateUser status='add' />
+              <CreateUser status='add' addnewuser ={addUser} />
           </Route>
           <Route path='/edit/:id' children={<CreateUser status='edit' />}></Route>
           <Route path='*'>
--- react-test/src/components/CreateUser.js
+++ react-test/src/components/CreateUser.js
@@ -1,23 +1,47 @@
+import { useState } from "react";
 import { useParams } from "react-router";
+import { useHistory } from "react-router";
 
-
-
-const CreateUser = ({status})=>{
+const CreateUser = ({status,addnewuser})=>{
     const {id:eid} = useParams();
+    const [user, setUser] = useState({name: '', id:'', dept:''});
+    const history = useHistory();
+        
+    const change = (e)=>{
+        //console.log(e.target.value);
+        const attr = e.target.name;
+        const val = e.target.value;
+
+
+        setUser({...user,[attr]: val})
+    };
+    
+    const onsubmit = (e)=>{
+        e.preventDefault();
+       // console.log(e.target);
+        console.log(user);
+        addnewuser(user);
+        history.push('/userlist')
+        
+    }
+       
+
+   /*  function onsubmit(e) {
+        e.preventDefault();
+        alert('You clicked submit.');
+    } */
     return(
         <>
             <br/>
             <h3>{status==='add'?'Create':'Edit'} User Page: {eid}</h3>
-            <form onSubmit={} >
-                Name: <input type='text' name='name'/> <br/>
-                ID: <input type='text' name='id'  /><br/>
-                Dept: <input type='text' name='dept' /><br/>
+            <form onSubmit = {onsubmit}>
+                Name: <input type='text' name='name' value={user.name} onChange={change}/> <br/>
+                ID: <input type='text' name='id'  value={user.id} onChange={change} /><br/>
+                Dept: <input type='text' name='dept' value={user.dept} onChange={change} /><br/>
                 <input type='submit' value={status==='add'?'Create':'Update'}/>
             </form>
         </>
     );
 }
-
-
 
 export default CreateUser;
